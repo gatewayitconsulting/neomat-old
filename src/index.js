b4c2462064d54ae4884edde9849219144c4f950b
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2021-05-12 20:45:05 -0500

    Implement Basic Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+      <Switch>
+        <Route exact path="/"><App /></Route>
+        {/* <Route path="/category"><Category /></Route> */}
+        {/* <Route path="/products"><Products /></Route> */}
+        {/* <Route path="/*"><ErrorPage /></Route> */}
+      </Switch>
+    </BrowserRouter>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
